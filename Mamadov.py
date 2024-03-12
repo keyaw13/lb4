8c7d8326b3d594bab3a4741aa009aa3d3d2425cb
admin330077 <162129803+admin330077@users.noreply.github.com>
2024-03-12 11:02:17 +0300

    Update Mamadov.py
    складывают

--- Mamadov.py
+++ Mamadov.py
@@ -3,7 +3,7 @@ def calculate_sum(n):
     sum2 = sum([i**7 for i in range(1, n+1)])
     sum3 = sum([i for i in range(1, n+1)])
     result = 2 * (sum3**4)
-    return sum1 + sum2, result
+    return sum1 + sum2, result #складывают числа
 
 N = 10  # Задайте значение N
 result_sum, expected_result = calculate_sum(N)
